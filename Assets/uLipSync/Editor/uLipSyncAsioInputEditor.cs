26c3e3cf96785e0d9142e49c43c384e783dc0672
Hidano <n.hidano@hidano-dev.com>
2026-04-15 09:13:05 +0900

    6.2 インスペクタのステータス表示・デバイス更新ボタン・Start/Stop ボタンを実装する
    Modified: Assets/uLipSync/Editor/uLipSyncAsioInputEditor.cs
    - isRecording状態、選択デバイス名、最終コールバックサンプル数を読み取り専用フィールドとして表示
    - デバイス更新ボタンを追加し、押下時にAsioOut.GetDriverNames()を再実行してリスト更新
    - ドライバ0件時に警告ヘルプボックスを表示
    - isAutoStart=false かつ再生中の場合にStart/Stop Recordingボタンを表示
    - 再生中はRepaint()で自動リフレッシュ

--- Assets/uLipSync/Editor/uLipSyncAsioInputEditor.cs
+++ Assets/uLipSync/Editor/uLipSyncAsioInputEditor.cs
@@ -9,6 +9,15 @@ public class uLipSyncAsioInputEditor : Editor
 {
     uLipSyncAsioInput asio => target as uLipSyncAsioInput;
 
+    string[] _driverNames = new string[0];
+
+    void OnEnable()
+    {
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+        RefreshDriverList();
+#endif
+    }
+
     public override void OnInspectorGUI()
     {
 #if !UNITY_STANDALONE_WIN && !UNITY_EDITOR_WIN
@@ -17,10 +26,73 @@ public class uLipSyncAsioInputEditor : Editor
             MessageType.Warning);
 #else
         serializedObject.Update();
+
         DrawDefaultInspector();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Status", EditorStyles.boldLabel);
+
+        using (new EditorGUI.DisabledGroupScope(true))
+        {
+            EditorGUILayout.Toggle("Is Recording", asio.isRecording);
+            EditorGUILayout.TextField("Selected Device", asio.selectedDeviceName ?? "(none)");
+            EditorGUILayout.IntField("Last Callback Samples", asio.lastCallbackSampleCount);
+        }
+
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("デバイス更新"))
+        {
+            RefreshDriverList();
+        }
+
+        if (_driverNames.Length == 0)
+        {
+            EditorGUILayout.HelpBox(
+                "ASIO ドライバが見つかりません。ASIO ドライバがインストールされているか確認してください。",
+                MessageType.Warning);
+        }
+
+        if (!serializedObject.FindProperty("isAutoStart").boolValue && EditorApplication.isPlaying)
+        {
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Manual Control", EditorStyles.boldLabel);
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                using (new EditorGUI.DisabledGroupScope(asio.isRecording))
+                {
+                    if (GUILayout.Button("Start Recording"))
+                    {
+                        asio.StartRecord();
+                    }
+                }
+
+                using (new EditorGUI.DisabledGroupScope(!asio.isRecording))
+                {
+                    if (GUILayout.Button("Stop Recording"))
+                    {
+                        asio.StopRecord();
+                    }
+                }
+            }
+        }
+
         serializedObject.ApplyModifiedProperties();
+
+        if (EditorApplication.isPlaying)
+        {
+            Repaint();
+        }
 #endif
     }
+
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+    void RefreshDriverList()
+    {
+        _driverNames = asio.GetAsioDriverNames();
+    }
+#endif
 }
 
 }
